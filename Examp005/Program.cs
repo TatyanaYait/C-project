ed963925c03f8b8a8015dd11da708b6c1f77c6b7
Tatyana <Bagira-mur-rr@mail.ru>
2022-08-18 01:39:02 +0600

    Examp005 sum Random

---
+++ Examp005/Program.cs
@@ -0,0 +1,6 @@
+﻿int numbA = new Random().Next(1, 10);   
+Console.WriteLine(numbA);
+int numbB = new Random().Next(1, 10);
+Console.WriteLine(numbB);
+int result = numbA + numbB;
+Console.WriteLine(result);
